fdadee01a2cf13f5feb94e88fbc4c6e78e2da5be
abobr-itra <bobrandrey1972@gmail.com>
2022-12-02 16:04:45 +0300

    Add extensions for Double and Optional

---
+++ WeatherApp/WeatherApp/Extensions/Optional+Extensions.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension Optional {
+  public func `do`(_ action: (Wrapped) -> Void) {
+    self.map(action)
+  }
+  
+  public func `do`(_ action: ((Wrapped) -> Void)?) {
+    action.do(self.do)
+  }
+}
+
