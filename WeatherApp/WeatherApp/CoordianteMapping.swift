7010d66a54caba16da7838ec970cde2f20db9853
abobr-itra <bobrandrey1972@gmail.com>
2022-12-05 14:21:33 +0300

    Update CoordinateMigration class

--- WeatherApp/WeatherApp/CoordianteMapping.swift
+++ WeatherApp/WeatherApp/CoordianteMapping.swift
@@ -6,12 +6,6 @@ class CoordinateMigration: NSEntityMigrationPolicy {
   override func createDestinationInstances(forSource sInstance: NSManagedObject,
                                            in mapping: NSEntityMapping,
                                            manager: NSMigrationManager) throws {
-
-//    if sInstance.entity.name == "CountryEntity" {
-//      let latlng = sInstance.primitiveValue(forKey: "latlng") as! [Double]
-//      setUpCoordinate(latlng[0], latlng[1], manager: manager)
-//    }
-    
     if sInstance.entity.name == "WeatherDataEntity" || sInstance.entity.name == "CityEntity" {
       let lat = sInstance.primitiveValue(forKey: "lat") as? Double
       let lon = sInstance.primitiveValue(forKey: "lon") as? Double
@@ -19,7 +13,7 @@ class CoordinateMigration: NSEntityMigrationPolicy {
                                                            into: manager.destinationContext)
       coordinate.setValue(lat, forKey: "lat")
       coordinate.setValue(lon, forKey: "lon")
-      sInstance.entity.setValue(coordinate, forKey: "coordinate")
+      sInstance.entity.setValue(coordinate.entity, forKey: "coordinate")
     }
   }
   
