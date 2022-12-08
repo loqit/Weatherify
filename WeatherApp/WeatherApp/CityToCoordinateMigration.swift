5a45f70cae2745026ecfc75865ed65f07f28c0fb
abobr-itra <bobrandrey1972@gmail.com>
2022-12-08 10:48:40 +0300

    Create separate mapping policy classes for weather and city

--- WeatherApp/WeatherApp/CityToCoordinateMigration.swift
+++ WeatherApp/WeatherApp/CityToCoordinateMigration.swift
@@ -1,21 +1,15 @@
-import Foundation
 import CoreData
 
-class CoordinateMigration: NSEntityMigrationPolicy {
-
+class CityToCoordinateMigration: NSEntityMigrationPolicy {
   override func createDestinationInstances(forSource sInstance: NSManagedObject,
                                            in mapping: NSEntityMapping,
                                            manager: NSMigrationManager) throws {
-    
     let lat = sInstance.primitiveValue(forKey: "lat") as? Double
     let lon = sInstance.primitiveValue(forKey: "lon") as? Double
     let coordinate = NSEntityDescription.insertNewObject(forEntityName: "CoordinateEntity",
                                                          into: manager.destinationContext)
     coordinate.setValue(lat, forKey: "lat")
     coordinate.setValue(lon, forKey: "lon")
-    print("sInstance entity :", sInstance)
-    print("mapping : ", manager.currentEntityMapping)
-    
     let entities = manager.destinationInstances(forEntityMappingName: mapping.name,
                                                 sourceInstances: [sInstance])
     print("Entities", entities)
